af016aacc7d3c99458457293f75b99325c2ebded
SadLaboka <jitubuh@gmail.com>
2022-07-01 18:01:40 +0300

    add solution of task from the lecture2

---
+++ tests/test_lecture2.py
@@ -0,0 +1,31 @@
+from ylab_tasks.lecture2 import distance, shortest_route
+
+
+def test_distance():
+    assert distance((0, 0), (0, 0)) == 0
+    assert distance((0, 0), (1, 0)) == 1
+
+
+def test_shortest_route():
+    points = ((1, 0), (2, 0), (3, 0), (4, 0), (5, 0))
+    points2 = ((0, 2), (2, 5), (5, 2), (6, 6), (8, 3))
+    points3 = ((0, 1), (1, 4), (4, 1), (5, 5), (7, 2))
+
+    many_result = "Too many points. Max number of points 7"
+    few_result = "Too few points. Min number of points 2"
+    result = "(1, 0) -> (2, 0)[1.0] -> (3, 0)[1.0] -> (4, 0)" \
+             "[1.0] -> (5, 0)[1.0] -> (1, 0)[4.0] = 8.0"
+    result2 = "(0, 2) -> (2, 5)[3.6055512754639896] -> (5, 2)" \
+              "[4.242640687119286] -> (6, 6)[4.123105625617661] -> (8, 3)" \
+              "[3.6055512754639896] -> (0, 2)[8.06225774829855] = " \
+              "23.639106611963477"
+    result3 = "(0, 1) -> (1, 4)[3.1622776601683795] -> (4, 1)" \
+              "[4.242640687119286] -> (5, 5)[4.123105625617661] -> (7, 2)" \
+              "[3.6055512754639896] -> (0, 1)[7.0710678118654755] = " \
+              "22.204643060234794"
+
+    assert shortest_route(((0, 0),)) == few_result
+    assert shortest_route(points + ((6, 0), (7, 0), (8, 0))) == many_result
+    assert shortest_route(points) == result
+    assert shortest_route(points2) == result2
+    assert shortest_route(points3) == result3
---
+++ ylab_tasks/lecture2.py
@@ -0,0 +1,36 @@
+"""Solution of task from the second lecture"""
+from itertools import combinations
+from math import hypot
+
+
+def distance(p1: tuple, p2: tuple) -> float:
+    """Calculates the distance between 2 points"""
+    return hypot(p2[0] - p1[0], p2[1] - p1[1])
+
+
+def shortest_route(points: tuple) -> str:
+    """Calculates the shortest route between all points"""
+    number_of_points = len(points)
+
+    if number_of_points > 7:
+        return "Too many points. Max number of points 7"
+    elif number_of_points <= 1:
+        return "Too few points. Min number of points 2"
+
+    end_point = points[0]
+    shortest = []
+
+    for variation in combinations(points[1:], number_of_points - 1):
+        route_length = 0
+        last_point = end_point
+        result = f"{last_point}"
+
+        for point in (variation + (end_point, )):
+            dist = distance(last_point, point)
+            route_length += dist
+            result += f" -> {point}[{dist}]"
+            last_point = point
+        if not shortest or shortest[0] > route_length:
+            shortest = [route_length, result]
+
+    return f"{shortest[1]} = {shortest[0]}"
