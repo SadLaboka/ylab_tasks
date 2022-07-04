d2b0abffe3dfd78686db17215cb01d92646a7f7d
SadLaboka <jitubuh@gmail.com>
2022-07-04 15:40:21 +0300

    update task2 from the lecture2

--- ylab_tasks/lecture2/task2.py
+++ ylab_tasks/lecture2/task2.py
@@ -89,58 +89,84 @@ def click(row, col):
         crosses.add((row, col))
         global cross_count
         cross_count += 1
-        change_lose_line(check_lose(crosses))
+        change_lose_line(check_lose(crosses, 5))
         if game_run and cross_count < 50:
             computer_move()
         elif cross_count >= 50:
             draw_draw()
 
 
-def computer_move():
+def computer_move(): # noqa
     """Creates a computer move"""
-    count = 0
+    attempts_count = 0
+    lose_attempts = 0
+    hard_checks_count = 0
     difficulty_map = {
-        "Easy": 1,
-        "Medium": 7,
-        "Hard": 15
+        "Easy": {"checks": 2, "depth": 4, "hard": False},
+        "Medium": {"checks": 10, "depth": 4, "hard": False},
+        "Hard": {"checks": 20, "depth": 3, "hard": True}
     }
+    checks_count = difficulty_map[difficulty]["checks"]
+    inspection_depth = difficulty_map[difficulty]["depth"]
+    hard_mode = difficulty_map[difficulty]["hard"]
+    if len(zeroes) > 30:
+        hard_mode = False
+
     while True:
         row = random.randint(0, 9)
         col = random.randint(0, 9)
+        attempt = {(row, col)}
+
         if field[row][col]['text'] == ' ':
-            lose_combination = check_lose(zeroes | {(row, col)})
-            if lose_combination and count < difficulty_map[difficulty]:
-                count += 1
+
+            # Checks neighboring cells
+            if hard_mode and hard_checks_count < 10:
+                neighboring_cells = check_lose(zeroes | crosses | attempt, 2)
+                if neighboring_cells:
+                    hard_checks_count += 1
+                    continue
+
+            # Checks the square.
+            # The size of the square depends on the difficulty level
+            lose_combination = check_lose(zeroes | attempt, inspection_depth)
+            if lose_combination and attempts_count < checks_count:
+                hard_checks_count = 0
+                attempts_count += 1
                 continue
-            else:
-                change_lose_line(lose_combination)
-                field[row][col]['text'] = 'O'
-                field[row][col]['fg'] = 'blue'
-                zeroes.add((row, col))
-                break
+
+            lose = check_lose(zeroes | attempt, 5)
+            if lose and lose_attempts < checks_count:
+                lose_attempts += 1
+                continue
+
+            change_lose_line(lose)
+            field[row][col]['text'] = 'O'
+            field[row][col]['fg'] = 'blue'
+            zeroes.add((row, col))
+            break
 
 
-def check_lose(cells: set) -> set:
+def check_lose(cells: set, count: int) -> set:
     """Checks for defeat conditions"""
     global game_run
     for cell in cells:
 
-        tmp_cells = check_vertical(cell, cells)
+        tmp_cells = check_vertical(cell, cells, count)
         if len(tmp_cells) >= 4:
             tmp_cells.add(cell)
             return tmp_cells
 
-        tmp_cells = check_horizontal(cell, cells)
+        tmp_cells = check_horizontal(cell, cells, count)
         if len(tmp_cells) >= 4:
             tmp_cells.add(cell)
             return tmp_cells
 
-        tmp_cells = check_first_diagonal(cell, cells)
+        tmp_cells = check_first_diagonal(cell, cells, count)
         if len(tmp_cells) >= 4:
             tmp_cells.add(cell)
             return tmp_cells
 
-        tmp_cells = check_second_diagonal(cell, cells)
+        tmp_cells = check_second_diagonal(cell, cells, count)
         if len(tmp_cells) >= 4:
             tmp_cells.add(cell)
             return tmp_cells
@@ -148,17 +174,17 @@ def check_lose(cells: set) -> set:
     return set()
 
 
-def check_vertical(cell: tuple, cells: set) -> set:
+def check_vertical(cell: tuple, cells: set, count: int) -> set:
     """Checks the cell vertical"""
     tmp_cells = set()
-    for i in range(1, 5):
+    for i in range(1, count):
         bot_cell = (cell[0] + i, cell[1])
         if bot_cell in cells:
             tmp_cells.add(bot_cell)
         else:
             break
 
-    for i in range(1, 5):
+    for i in range(1, count):
         top_cell = (cell[0] - i, cell[1])
         if top_cell in cells:
             tmp_cells.add(top_cell)
@@ -168,17 +194,17 @@ def check_vertical(cell: tuple, cells: set) -> set:
     return tmp_cells
 
 
-def check_horizontal(cell: tuple, cells: set) -> set:
+def check_horizontal(cell: tuple, cells: set, count: int) -> set:
     """Checks the cell horizontal"""
     tmp_cells = set()
-    for i in range(1, 5):
+    for i in range(1, count):
         right_cell = (cell[0], cell[1] + i)
         if right_cell in cells:
             tmp_cells.add(right_cell)
         else:
             break
 
-    for i in range(1, 5):
+    for i in range(1, count):
         left_cell = (cell[0], cell[1] + i)
         if left_cell in cells:
             tmp_cells.add(left_cell)
@@ -188,7 +214,7 @@ def check_horizontal(cell: tuple, cells: set) -> set:
     return tmp_cells
 
 
-def check_first_diagonal(cell: tuple, cells: set) -> set:
+def check_first_diagonal(cell: tuple, cells: set, count: int) -> set:
     """Checks diagonal top-left to bot-right"""
     tmp_cells = set()
     for i in range(1, 5):
@@ -198,7 +224,7 @@ def check_first_diagonal(cell: tuple, cells: set) -> set:
         else:
             break
 
-    for i in range(1, 5):
+    for i in range(1, count):
         top_left_cell = (cell[0] - i, cell[1] - i)
         if top_left_cell in cells:
             tmp_cells.add(top_left_cell)
@@ -208,17 +234,17 @@ def check_first_diagonal(cell: tuple, cells: set) -> set:
     return tmp_cells
 
 
-def check_second_diagonal(cell: tuple, cells: set) -> set:
+def check_second_diagonal(cell: tuple, cells: set, count: int) -> set:
     """Checks diagonal bot-left to top-right"""
     tmp_cells = set()
-    for i in range(1, 5):
+    for i in range(1, count):
         bot_left_cell = (cell[0] + i, cell[1] - i)
         if bot_left_cell in cells:
             tmp_cells.add(bot_left_cell)
         else:
             break
 
-    for i in range(1, 5):
+    for i in range(1, count):
         top_right_cell = (cell[0] - i, cell[1] + i)
         if top_right_cell in cells:
             tmp_cells.add(top_right_cell)
